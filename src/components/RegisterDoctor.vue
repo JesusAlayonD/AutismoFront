d62a67bd480fb38065d0aae222ed79c000e24e38
JesusAlayonD <jesusalayond@gmail.com>
2023-06-04 20:59:27 -0600

    Errors in register

--- src/components/RegisterDoctor.vue
+++ src/components/RegisterDoctor.vue
@@ -2,6 +2,7 @@
     import axios from 'axios';
     import { doctorStore } from '../../store/doctor.js'
     import { useRoute, useRouter } from "vue-router";
+    import { ref, computed } from "vue";
 
     const router = useRouter();
 
@@ -9,10 +10,18 @@
     let lastName
     let email
     let password
+    let confirmPassword
     let sex
 
+    const error = ref(false)
+    const errorP = ref(false)
+
     const register = async() => {
-        const data = {
+
+        if(password === confirmPassword)
+        {
+            errorP.value = false
+            const data = {
             name: name,
             last_name: lastName,
             email: email,
@@ -22,8 +31,33 @@
 
         await doctorStore().register(data)
 
-        router.push({ name: "patientsInfo"})
+        if (doctorStore().data.name) {
+            router.push({ name: "patientsInfo"})
+        } else {
+            error.value = true
+        }
+        } else {
+            errorP.value = true
+        }
+        
     }
+
+    const errorHandle = computed(() => {
+        if(error.value){
+            return "visible"
+        }else{
+            return "hidden"
+        }
+      })
+
+      const errorHandleP = computed(() => {
+        if(errorP.value){
+            return "visible"
+        }else{
+            return "hidden"
+        }
+      })
+
 </script>
 
 <template>
@@ -54,8 +88,11 @@
                     class="h-8 w-full rounded-md border border-slate-300 text-sm pl-2 bg-transparent outline-blue-600 shadow-sm">
                 <!-- confirm password -->
                 <label for="confirmPassword" class="text-sm">Confirmar Contraseña</label><br>
-                <input type="password" name="" id="confirmPassword" 
+                <input type="password" name="" id="confirmPassword" v-model="confirmPassword"
                     class="h-8 w-full rounded-md border border-slate-300 text-sm pl-2 bg-transparent outline-blue-600 shadow-sm">
+                <div :class="errorHandleP" class="my-4 text-sm bg-red-100 border border-red-500 text-red-700 px-4 py-3 rounded relative" role="alert">
+                <span class="block sm:inline">Las contraseñas no coinciden.</span>
+                </div>
                 <!-- radio buttons for gender -->
                 <div id="gender" class="text-sm mb-2">
                     <p class="mt-2">Sexo</p>
@@ -63,9 +100,12 @@
                     <input type="radio" name="gender" id="female" class="text-sm mx-1" v-model="sex" value="F"><label for="female">F</label>
                     <input type="radio" name="gender" id="other" class="text-sm mx-1" v-model="sex" value="Other"><label for="other">Other</label>
                 </div>
+                <div :class="errorHandle" class="mb-4 text-sm bg-red-100 border border-red-500 text-red-700 px-4 py-3 rounded relative" role="alert">
+                <span class="block sm:inline">El correo ya está registrado.</span>
+                </div>
                 <!-- Sign up / submit button -->
                 <input type="submit" name="" id="signUp" value="Registrarse"
                     class="bg-blue-700 w-full h-10 cursor-pointer text-white rounded-md hover:bg-blue-600 hover:outline outline-2 outline-blue-600 outline-offset-2 text-sm"><br>
-                <p class="text-xs my-2">¿Ya tienes una cuenta? <a href="#" class="text-blue-600">Inicia Sesión</a></p>
+                    <RouterLink to="/login" class="text-xs my-2">¿Ya tienes una cuenta? <a href="#" class="text-blue-600">Inicia Sesión</a></RouterLink>
             </form>
 </template>
--- src/components/RegisterPatient.vue
+++ src/components/RegisterPatient.vue
@@ -1,7 +1,8 @@
 <script setup>
     import axios from 'axios';
+    import { ref, computed } from "vue";
     import { patientStore } from '../../store/patient.js'
-    import { useRoute, useRouter } from "vue-router";
+    import { useRoute, useRouter, RouterLink } from "vue-router";
 
     const router = useRouter();
 
@@ -10,11 +11,19 @@
     let patientName
     let email
     let password
+    let confirmPassword
     let sex
     let age
 
+    const error = ref(false)
+    const errorP = ref(false)
+
     const register = async() => {
-        const data = {
+
+        if(password === confirmPassword){
+            errorP.value = false
+
+            const data = {
             name: name,
             last_name: lastName,
             patientName: patientName,
@@ -26,9 +35,34 @@
 
         await patientStore().register(data)
 
-        router.push({ name: "test"})
+        if (patientStore().data.name) {
+            router.push({ name: "test"})
+        } else {
+            error.value = true
+        }
+        } else {
+            errorP.value = true
+        }
+
+        
       }
-    
+
+      const errorHandle = computed(() => {
+        if(error.value){
+            return "visible"
+        }else{
+            return "hidden"
+        }
+      })
+
+      const errorHandleP = computed(() => {
+        if(errorP.value){
+            return "visible"
+        }else{
+            return "hidden"
+        }
+      })
+
 </script>
 
 <template>
@@ -72,10 +106,14 @@
                 <label for="password" class="text-sm">Contraseña</label><br>
                 <input type="password" name="" id="password" v-model="password"
                     class="h-8 w-full rounded-md border border-slate-300 text-sm pl-2 bg-transparent outline-blue-600 shadow-sm">
+                
                 <!-- confirm password -->
                 <label for="confirmPassword" class="text-sm">Confirmar Contraseña</label><br>
-                <input type="password" name="" id="confirmPassword" 
+                <input type="password" name="" id="confirmPassword" v-model="confirmPassword"
                     class="h-8 w-full rounded-md border border-slate-300 text-sm pl-2 bg-transparent outline-blue-600 shadow-sm">
+                <div :class="errorHandleP" class="my-4 text-sm bg-red-100 border border-red-500 text-red-700 px-4 py-3 rounded relative" role="alert">
+                <span class="block sm:inline">Las contraseñas no coinciden.</span>
+                </div>
                 <!-- radio buttons for gender -->
                 <div id="gender" class="text-sm mb-2">
                     <p class="mt-2">Sexo</p>
@@ -83,9 +121,12 @@
                     <input type="radio" name="gender" id="female" class="text-sm mx-1" v-model="sex" value="F"><label for="female">F</label>
                     <input type="radio" name="gender" id="other" class="text-sm mx-1" v-model="sex" value="Other"><label for="other">Other</label>
                 </div>
+                <div :class="errorHandle" class="mb-4 text-sm bg-red-100 border border-red-500 text-red-700 px-4 py-3 rounded relative" role="alert">
+                <span class="block sm:inline">El correo ya está registrado.</span>
+                </div>
                 <!-- Sign up / submit button -->
                 <input type="submit" name="" id="signUp" value="Registrarse"
                     class="bg-blue-700 w-full h-10 cursor-pointer text-white rounded-md hover:bg-blue-600 hover:outline outline-2 outline-blue-600 outline-offset-2 text-sm"><br>
-                <p class="text-xs my-2">¿Ya tienes una cuenta? <a href="#" class="text-blue-600">Inicia Sesión</a></p>
+                <RouterLink to="/login" class="text-xs my-2">¿Ya tienes una cuenta? <a href="#" class="text-blue-600">Inicia Sesión</a></RouterLink>
             </form>
 </template>
